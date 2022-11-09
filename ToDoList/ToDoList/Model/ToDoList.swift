44a29d95eac642e2d2c669f8f85ff334377a2c6a
kwhn9486@naver.com <kwhn9486@naver.com>
2022-11-09 23:25:11 +0900

    complete

--- ToDoList/ToDoList/Model/ToDoList.swift
+++ ToDoList/ToDoList/Model/ToDoList.swift
@@ -7,17 +7,11 @@
 
 import SwiftUI
 
-struct ToDoList : Codable, Identifiable {
-    
-    var id: UUID = UUID()
-    var date: Date
-    var checkBoxToDos: [CheckBoxToDo]
-}
-//date를 기준으로 section을 만들어서 할일 목록 리스트들을 나열한다
-
 struct CheckBoxToDo: Codable, Hashable, Identifiable {
     var id: UUID = UUID()
-    var checkBox: Bool
-    var toDo: String
+    var dates: Date
+    var toDo: [String : Bool]
+    var tasks: [String]
 }
+
 //체크박스랑 + 할일 같이 있는 목록
